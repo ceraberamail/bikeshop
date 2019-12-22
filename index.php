37de3f6f4765d1f46f49b4dd680b19d7a183730f
ceraberamail <ceraberamail@gmail.com>
2019-12-22 07:19:06 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+     <html lang="en">
+         <head>
+             <meta charset="UTF-8">
+            <title>Hello BikeShop Test Page</title>
+         </head>
+         <body>
+             <h1>Hello BikeShop!</h1>
+         </body>
+    </html>
