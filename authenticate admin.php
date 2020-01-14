8696b7222d65fb74601b96de914a0e71dd43cac0
kohlercsabi <csaba.kohler@gmail.com>
2020-01-14 18:59:49 +0100

    admin belépéshez authenticate admin.php hozzáadása

---
+++ authenticate admin.php
@@ -0,0 +1,53 @@
+<?php
+
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'root';
+$DATABASE_NAME = 'bikeshop';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+    die ('Please fill both the username and password field!');}
+
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM admins WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+    $stmt->store_result();
+    
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        // Note: remember to use password_hash in your registration file to store the hashed passwords.
+        if ($_POST['password'] === $password) {
+            // Verification success! User has loggedin!
+            // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+
+            
+            echo 'Welcome ' . $_SESSION['username'] . '!'. ' You have logged in as an administrator!' . ' You will be redirected to Add page!';
+            header("Refresh: 3;url= add.php");exit;
+
+        } else {
+            echo 'Incorrect password!';
+            header("Refresh: 3;url= sign_in.php");exit;
+        }
+    } else {
+        echo 'Incorrect username!';}
+        header("Refresh: 3;url= sign_in.php");exit;
+
+    $stmt->close();}      
+?>
