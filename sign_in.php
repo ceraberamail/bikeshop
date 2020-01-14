3e7e1e964e189d53fb6105216cf5d66c563bd14a
kohlercsabi <csaba.kohler@gmail.com>
2020-01-14 19:01:10 +0100

    sign in as administrator hozzáadása

--- sign_in.php
+++ sign_in.php
@@ -20,6 +20,23 @@
 
 </section> 
 
+<section class="container grey-text">
+    <h4 class="center">Sign in as administrator</h4>
+    <form action="authenticate admin.php" class="white" method="POST">
+        
+    <label for="username">
+					<i class="fas fa-user"></i>
+				</label>
+				<input type="text" name="username" placeholder="Username" id="username" required>
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Password" id="password" required>
+				<input type="submit" value="Login">
+	</form>
+
+</section> 
+
 <?php  include('templates/footer.php'); ?>
 
 </body>
